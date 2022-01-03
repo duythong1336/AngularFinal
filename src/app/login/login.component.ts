08038fd3d09e6e7e5cb3067a8cd3c2574c208a23
thong <thongngan1336@gmail.com>
2022-01-03 16:42:14 +0700

    add config

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -2,46 +2,48 @@ import { Component, OnInit } from '@angular/core';
 import Swal from 'sweetalert2';
 import { AuthService } from '../services/auth.service';
 import { FormBuilder, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
-  loginFrm = this.fb.group({
+  loginForm = this.fb.group({
     Email: ['', [Validators.email, Validators.required]],
     Password: ['', [Validators.required]],
   });
-  constructor(private authService: AuthService, private fb: FormBuilder) {}
+  constructor(private authService: AuthService, private fb: FormBuilder, private router: Router) {}
 
   ngOnInit(): void {}
 
   async tryGoogleLogin() {
-    try {
-      await this.authService.signinGmail()
-      console.log('login successfuly')
-    }
-    catch (err) {
-      console.log(err);
+    // try {
+    //   await this.authService.signinGmail()
+    //   console.log('login successfuly')
+    // }
+    // catch (err) {
+    //   console.log(err);
       
       
-    }
+    // }
     // console.log('abcd')
-    // this.authService.signinGmail().then((res) => {
+    this.authService.signinGmail().then((res) => {
       
-    //   Swal.fire({
-    //     position: 'top-end',
-    //     icon: 'success',
-    //     title: 'Login successful',
-    //     showConfirmButton: false,
-    //     timer: 1500,
-    //   });
-    //   location.href = '/admin/product';
-    // });
+      Swal.fire({
+        position: 'top-end',
+        icon: 'success',
+        title: 'Login successful',
+        showConfirmButton: false,
+        timer: 1500,
+      });
+      this.router.navigate(['/admin'])
+      // location.href = '/admin/product';
+    });
   }
-  tryEmailPasswordLogin() {
-    var email = this.loginFrm.controls['Email'].value;
-    var password = this.loginFrm.controls['Password'].value;
+  LoginbyEmailandPass() {
+    var email = this.loginForm.controls['Email'].value;
+    var password = this.loginForm.controls['Password'].value;
     this.authService
       .signinFirebase(email, password)
       .then((res) => {
@@ -52,7 +54,7 @@ export class LoginComponent implements OnInit {
           showConfirmButton: false,
           timer: 1500,
         });
-        location.href = '/admin/product';
+        this.router.navigate(['/admin'])
       })
       .catch((err) => {
         Swal.fire(err.message, '', 'error');
